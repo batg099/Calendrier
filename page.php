5b732509d7a277a51e7a875112315f16a9083dab
batg099 <109592365+batg099@users.noreply.github.com>
2024-07-08 16:22:02 +0200

    Bouton Scroll UP

--- page.php
+++ page.php
@@ -14,18 +14,35 @@
     <link rel="stylesheet" type="text/css" media="screen" href="./page.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
     
 </head>
-<body id="top">
-
+<body>
+<!--   Bouton Scroll UP   -->
+<div id="scrollUp">
+<a href="#top"><img src="to_top.png"/></a>
+</div>
+<script>
+jQuery(function(){
+    $(function () {
+        $(window).scroll(function () { //Fonction appelée quand on descend la page
+            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
+                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
+            } else { 
+                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+            }
+        });
+    });
+});
+</script>
+<!-- FIN Scroll UP -->
 <?php
 
 ('Content-Type: text/html; charset=utf-8');
    ini_set('display_errors',1);
    ini_set('display_startup_errors',1);
    error_reporting(E_ALL);
-   $open = fopen("./Produits.csv","r");
-   $open = fopen("./liste.csv","r");
+   $open = fopen("./uploads/Produits.csv","r");
    if (isset($_GET['param'])){
     $url = $_GET['param'];
    }
@@ -49,7 +66,7 @@
    $i=0;
    $limite = 0; // A SUPPRIMER PLUS TARD
    while ( ($data = fgetcsv($open,2000,";")) == true && $i>=0  ){
-        if($i>4 && $limite == 0 && $data[0]=='SEBTEST'){
+        if($i>4 && $limite == 0 && $data[0]==$url){
             $titre = $data[1].' - '.$data[0];
             $info = $data[3].' jours'.' ('.$data[2].' heures)'.' | '.'Prix : '.$data[4].' €ht';
             $objectifs = $data[37];
@@ -82,13 +99,43 @@
         echo '</div>';
         
         echo "<div class='alignement_2'>";
-        if($data[41]==$data[42]){
-            echo '<p id="petit_titre" >'.$data[35].'</p>';
+    	
+   $open_liste = fopen("./liste.csv","r");
+   $image = '';
+   $j=0;
+   while ( ($data_2 = fgetcsv($open_liste,2000,";")) == true && $j>=0  ){
+    if($j>=1){
+        //echo $data_2[42].' ';
+        if($data_2[1] == $url){
+            echo $data_2[42].' /';
+            echo $data_2[41].' ';
+            if(stripos($data_2[41], $data_2[42]) !== false && $data_2[41] !=='Agile SCRUM'){
+            $image = './images/'.$data_2[42].'.png';
+            }
+            else {
+            
+                 switch($data_2[42]){
+                    case 'MICROSOFT':
+                        $image = './images/'.$data_2[42].'.png';
+                        break;
+                    case 'AGILE':
+                        $image = './images/AGILE-SCRUM'.'.png';
+			break;
+                    case 'VEEAM':
+                        $image = './images/VEEAM'.'.png';
+		        break;
+                    default:
+                        $image = './images/'.$data_2[42].'-'.$data_2[41].'.png';            
+		}
+	    }
         }
-        echo "<div class='img'> <img src='Aquali.png'  alt='Description de l'image' width='100' height='100'> </div>";
+    }    
+$j++;
+   }
+        echo "<div class='img'> <img src=$image  alt='Description de l'image' width='100' height='100'> </div>";
         echo "<div class='texte'>";
         if($data[35] !== '' && $data[35] !== null){
-            echo '<p id="petit_titre" >'.$data[35].'</p>';
+            echo '<p id="petit_titre" >'.str_replace('_x000d_', "", $data[35]).'</p>';
         }
         echo '</div>';
         echo '</div>';
@@ -100,7 +147,11 @@
                 <!-- <a href=#bouton_calendrier><button type="button" id="bouton_cal" ><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M216-96q-29.7 0-50.85-21.5Q144-139 144-168v-528q0-29 21.15-50.5T216-768h72v-96h72v96h240v-96h72v96h72q29.7 0 50.85 21.5Q816-725 816-696v528q0 29-21.15 50.5T744-96H216Zm0-72h528v-360H216v360Zm0-432h528v-96H216v96Zm0 0v-96 96Zm264.21 216q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Zm-156 0q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Zm312 0q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Zm-156 144q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Zm-156 0q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Zm312 0q-15.21 0-25.71-10.29t-10.5-25.5q0-15.21 10.29-25.71t25.5-10.5q15.21 0 25.71 10.29t10.5 25.5q0 15.21-10.29 25.71t-25.5 10.5Z"/></svg>  Voir les prochaines dates</button> </a>
                 -->
             </div>
-            
+                <script>
+        	 document.getElementById('bouton_devis').addEventListener('click', function() {
+           	 window.location.href = 'https://www.learneo.fr/formulaire-devis.html';
+        	});
+    		</script>
             <br>
             <br>
             
@@ -123,21 +174,18 @@
             <div class="v-line">
             </div>';
 
+	    echo '<div id="text">';
             // Contenu des sections
-            echo '<div id="text">';
             for ($j = 0; $j < count($menu); $j++) {
                 if ($titre !== '' && $contenu[$j]!=='') {
                     // Remplacer les espaces par des tirets dans les IDs
                     $id = str_replace(' ', '-', $menu[$j]);
                     echo '<h3 id="' . $id . '">' . $menu[$j] . '</h3>';
-                    echo '<p >' . $contenu[$j] . '</p>';
+                    echo '<p>' . str_replace('_x000d_', "", $contenu[$j]) . '</p>';
                     echo '<hr>';
                 }
             }
-            echo '<a class="fr-link fr-icon-arrow-up-fill fr-link--icon-left" href="#top">
-    Revenir en haut de page
-</a>';
-            echo '</div>';
+	    echo '</div>';
             echo '</div>';
             
             echo '<div class = page> ';
@@ -150,12 +198,13 @@
             <?php
             /**************** Dates  ***************/
             
+            $open = fopen("./liste.csv","r");
             $chaine = '';
             $tab = [];
             while ( ($data = fgetcsv($open,2000,";")) == true && $i>=0  ){
                 if($i>=1){
                     $date = explode('/',$data[3]);
-                    if($data[1] === 'F-C-CCNA' && $date[2]>=date('Y') && $date[1]>=date('m')){
+                    if($data[1] === $url && $date[2]>=date('Y') && $date[1]>=date('m')){
                         array_push($tab,$data[3]);
                     }
                 }
@@ -186,11 +235,8 @@
             ?>
             <div id='buttons'>
                 <br>
-                
+                <button type="button" id="bouton_inscription">Comment s'inscrire</button>
             </div>
-            <script>
-
-            </script>
 
             <br>
             <h3> Modalités d'accés </h3>
@@ -201,7 +247,6 @@
                 5 jours ouvrés (en moyenne) avant le début de la formation </p>
             <br>
             <p style="font-weight:bold;border:1px solid black;border-radius:10px 10px;margin-left:78%;padding-right:7px"> <a href='https://www.learneo.fr/accessibilite-handicap.html'>Accessibilité aux personnes en situation de handicap </a></p>
-    <script src="site.js" ></script>
-</body>
+    <script src="site.js" ></script></body>
 
 </html>

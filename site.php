9c35b30722dd58c887b24689a205fd36c5464446
batg099 <109592365+batg099@users.noreply.github.com>
2024-07-24 09:53:54 +0200

    Click

--- site.php
+++ site.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <!-- MÃ©ta-donnÃ©es pour dÃ©finir le jeu de caractÃ¨res et la mise Ã  l'Ã©chelle -->
+    <!-- MÃƒÂ©ta-donnÃƒÂ©es pour dÃƒÂ©finir le jeu de caractÃƒÂ¨res et la mise ÃƒÂ  l'ÃƒÂ©chelle -->
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <!-- Titre de la page -->
     <title>Calendrier - Learneo</title>
@@ -11,14 +11,14 @@
     <link rel="stylesheet" type="text/css" media="screen and (min-width:600px)" href="sitebureau.css">
     <link rel="stylesheet" type="text/css" media="screen and (max-width:600px)" href="filmmobile.css">
     -->
-    <link rel="stylesheet" type="text/css" media="screen" href="learneo.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="./styles/learneo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
      
 </head>
 <body>
     <div class="block" >
-    <!-- En-tÃªte de la page -->
+    <!-- En-tÃƒÂªte de la page -->
 
 
 
@@ -34,13 +34,13 @@
    ini_set('display_startup_errors',1);
    error_reporting(E_ALL);
     // On ouvre le fichier
-   $open = fopen("liste.csv","r");
-   $open_cle = fopen("Produits.csv","r");
+   $open = fopen("../uploads/liste.csv","r");
+   $open_cle = fopen("../uploads/Produits.csv","r");
 
     /********** Bannissements ********/
     $indesirable=[];
     $data_ban='';
-    $open_ban = fopen("bannissement.txt","r");
+    $open_ban = fopen("./settings/bannissement.txt","r");
     while(($data_ban = fgets($open_ban)) == true){
         // trim sert a enlever les \n
         array_push($indesirable,trim($data_ban));
@@ -62,7 +62,7 @@
             array_push($calendrier,["coco"]);
 
             $departement = $data[42];
-            // On vÃ©rifie si le departement est deja present dans le menu
+            // On vÃƒÂ©rifie si le departement est deja present dans le menu
             if(in_array($departement,$menu) == false){
                 array_push($menu,$departement);
             }
@@ -144,7 +144,7 @@ search_off
         </tr>
                 <?php  
                         $i=0;
-                        $open = fopen("liste.csv","r");
+                        $open = fopen("../uploads/liste.csv","r");
                         $test=[0,0,0,0];
                         $indice=0;
                         $numero = 0;
@@ -165,8 +165,9 @@ search_off
                             */
                             if(count($data[3]) == 1){
                                 echo "<tr>";
-                                echo "<td>". htmlspecialchars($data[1]) . "</td>";
-                                echo "<td>". htmlspecialchars($data[2]) . "</td>";
+                                echo "<td><a style='text-decoration:none;color:black;' href='https://webgescof.learneo.fr/formations/page.php?param=" . htmlspecialchars($data[1]) . "'>" . htmlspecialchars($data[1]) . "</a></td>";
+
+                                echo "<td><a style='text-decoration:none;color:black;' href='https://webgescof.learneo.fr/formations/page.php?param=" . htmlspecialchars($data[1]) . "'>" . htmlspecialchars($data[2]) . "</a></td>";
                                 $calcul = (int)($data[7])/7;
                                 if(gettype($calcul) === "double" ){
                                     echo "<td style='text-align: center;'>". $data[7]. " h"."</td>";
@@ -250,7 +251,7 @@ search_off
                                     }
                                 }
                                 
-                                // Ajouter des mois vides si nÃ©cessaire
+                                // Ajouter des mois vides si nÃƒÂ©cessaire
                                 for ($i = $mois; $i <= 12; $i++) {
                                     if (!isset($tab_mois[$i])) {
                                         $tab_mois[$i] = [];
@@ -267,16 +268,16 @@ search_off
                                         }
                                         else{
                                             //print_r($value);
-                                            $nombre = 0; // Va servir Ã  savoir s'il existe plusieurs dates dans le meme mois
+                                            $nombre = 0; // Va servir ÃƒÂ  savoir s'il existe plusieurs dates dans le meme mois
                                             usort($value,"cmp");
-                                            usort($value,"cmp_days"); // Sert Ã  trier selon le jour 
+                                            usort($value,"cmp_days"); // Sert ÃƒÂ  trier selon le jour 
                                             foreach($value as $date){
                                                 // On compte le nombre de date qu'on a dans le mois
                                                 if($date != ''){
                                                     $nombre = $nombre + 1;
                                                 }
                                                 $jour = explode("/",$date)[0];
-                                                // Sert Ã  Ã©viter la rÃ©pÃ©tition + On fait la difference entre les deux dates.
+                                                // Sert ÃƒÂ  ÃƒÂ©viter la rÃƒÂ©pÃƒÂ©tition + On fait la difference entre les deux dates.
                                                 
                                                     //echo "My date_1 is".$date;
                                                     //echo "My date_2 is".date('d-m-Y');
@@ -377,12 +378,35 @@ search_off
                             $i++;
 			}; 
 			
-			                     
+            $titre='';
+            $info='';
+            $objectifs ='';
+            $public='';
+            $requis='';
+            $programme='';
+            $methode='';
+            $eval='';
+            $certif='';
+            $menu = ['Objectifs','Pour qui ?','PrÃ©-Requis','Programme','PÃ©dagogie','Evaluation','Certification'];
+            $contenu = [];
+            $i=0;
+            $limite = 0; // A SUPPRIMER PLUS TARD
+            $tab_cle=[];
+            while ( ($data = fgetcsv($open_cle,2000,";")) == true && $i>=0  ){
+                 if($i>4 && $limite == 0 ){
+                    if(isset($data[36])){$certif = getStringBetween($data[36],'<ordaff>','</ordaff>'); }// A VERIFIER !!!!!
+                    if(stripos($data[0],'F-')!==false){
+                        //echo $data[0];
+                        array_push($tab_cle,[$data[0],$certif]);
+                    }
+                 }
+                 $i=$i+1;
+            }
 			
                         
                             array_sort_by_column($tab, 42);
 
-                            //Fonction qui sert Ã  comparer les dates de debut dans notre dictionnaire
+                            //Fonction qui sert ÃƒÂ  comparer les dates de debut dans notre dictionnaire
 
                             foreach($tab as $data){
                                 if($data[1]!=='F-AGCC'){
@@ -404,9 +428,11 @@ search_off
                                                 echo "<td colspan='9' class='table-heading'>".$valeur[0][42]."</td>" ;
                                                 echo "</tr>";
                                             }
+
                                             // On trie le tableau associe a chaque cle en fonction des dates de debuts
-                                            usort($valeur, "cmp");
-                                            //usort($valeur, "cmp_cle");
+                                            //usort($valeur, "cmp");
+                                            usort($valeur, "cmp_cle");
+
                                             foreach($valeur as $ligne){
                                                 //echo "Moshi Moshi".$valeur[0][41];
                                                 if($ligne[1]==='F-ITIL-FOUND'|| stripos($ligne[2], "TEST") == false &&  stripos($ligne[1], "TEST") === false && (stripos($ligne[1],'F-') !== false || stripos($ligne[1],'UCO-') !== false )&& $ligne[1]!=='F-AGCC' ){
@@ -421,8 +447,10 @@ search_off
                                                 echo "<td colspan='9' class='table-heading'>".$valeur[0][42]."</td>" ;
                                                 echo "</tr>";
                                             }
-					    usort($valeur, "cmp");
-					    //usort($valeur, "cmp_cle");
+
+                                            //usort($valeur, "cmp");
+                                            usort($valeur, "cmp_cle");
+
                                             foreach($valeur as $ligne){
                                                 $departement = $ligne[42];
                                                 // Recherche par Domaines
@@ -504,7 +532,7 @@ search_off
 				                    }
                                 }
                             }
-                            echo $valeur_a.' + '.$valeur_b.' | ';
+                            //echo $valeur_a.' + '.$valeur_b.' | ';
                             if ($valeur_a == $valeur_b) {
                                 return 0;
                             }
@@ -512,19 +540,19 @@ search_off
                             return ($valeur_a < $valeur_b) ? -1 : 1;
                         }
                         function differenceEnMois($date1, $date2) {
-                            // CrÃ©er des objets DateTime pour les deux dates
+                            // CrÃƒÂ©er des objets DateTime pour les deux dates
                             $datetime1 = DateTime::createFromFormat('d/m/Y', $date1);
                             $datetime2 = DateTime::createFromFormat('d/m/Y', $date2);
                         
-                            // VÃ©rifier si les objets DateTime ont Ã©tÃ© crÃ©Ã©s correctement
+                            // VÃƒÂ©rifier si les objets DateTime ont ÃƒÂ©tÃƒÂ© crÃƒÂ©ÃƒÂ©s correctement
                             if ($datetime1 === false || $datetime2 === false) {
                                 return "Erreur: Format de date invalide.";
                             }
                         
-                            // Calculer la diffÃ©rence entre les deux dates
+                            // Calculer la diffÃƒÂ©rence entre les deux dates
                             $interval = $datetime1->diff($datetime2);
                         
-                            // Calculer la diffÃ©rence totale en mois
+                            // Calculer la diffÃƒÂ©rence totale en mois
                             $diffEnMois = $interval->y * 12 + $interval->m;
                         
                             // Ajouter les mois partiels s'il y a plus de jours dans l'une des dates
@@ -533,6 +561,18 @@ search_off
                             }
                         
                             return $diffEnMois;
+                        }
+                        function getStringBetween($string, $start, $end) {
+                            $startPos = strpos($string, $start);
+                            if ($startPos === false) {
+                                return ''; // Si la chaÃ®ne de dÃ©part n'est pas trouvÃ©e, retourner une chaÃ®ne vide
+                            }
+                            $startPos += strlen($start); // Avancer le dÃ©but juste aprÃ¨s la chaÃ®ne de dÃ©part
+                            $endPos = strpos($string, $end, $startPos);
+                            if ($endPos === false) {
+                                return ''; // Si la chaÃ®ne de fin n'est pas trouvÃ©e, retourner une chaÃ®ne vide
+                            }
+                            return substr($string, $startPos, $endPos - $startPos);
                         }
                 ?>
    
@@ -545,7 +585,7 @@ search_off
     
 
     
-    <script src="site.js" ></script>
+    
 </body>
 
 </html>
